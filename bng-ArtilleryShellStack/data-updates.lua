7dc34e9cef6303b53c0461ae9f4a9172da707e12
Ben Gardner <bgardner@wabtec.com>
2023-09-09 21:29:51 -0500

    Add a copy of ArtilleryShellStack

---
+++ bng-ArtilleryShellStack/data-updates.lua
@@ -0,0 +1,6 @@
+data.raw.ammo["artillery-shell"].stack_size =
+  settings.startup["artillery-shell-stack-size"].value
+data.raw["artillery-turret"]["artillery-turret"].ammo_stack_limit =
+  settings.startup["artillery-turret-ammo-stack-limit"].value
+data.raw["artillery-wagon"]["artillery-wagon"].ammo_stack_limit =
+  settings.startup["artillery-wagon-ammo-stack-limit"].value
---
+++ bng-ArtilleryShellStack/settings.lua
@@ -0,0 +1,27 @@
+data:extend
+{
+  {
+    type = "int-setting",
+    name = "artillery-shell-stack-size",
+    setting_type = "startup",
+    default_value = 20,
+    minimum_value = 1,
+    order = "a"
+  },
+  {
+    type = "int-setting",
+    name = "artillery-turret-ammo-stack-limit",
+    setting_type = "startup",
+    default_value = 20,
+    minimum_value = 1,
+    order = "b"
+  },
+  {
+    type = "int-setting",
+    name = "artillery-wagon-ammo-stack-limit",
+    setting_type = "startup",
+    default_value = 20,
+    minimum_value = 1,
+    order = "c"
+  },
+}
