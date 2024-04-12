40912606654c4d71134f95c793d70d8f05ca2da5
ben <bng@system76-pc.localdomain>
2024-04-12 18:29:24 -0500

    Magic Science Check: add nullius support
    Cheat on!

---
+++ bng-magic-science-pack-chest/data-updates.lua
@@ -0,0 +1,7 @@
+local constants = require "src.constants"
+
+if mods["nullius"] then
+  local name = constants.CHEST_NAME
+  data.raw.item[name].order = "nullius-" .. name
+  data.raw.recipe[name].order = "nullius-" .. name
+end
